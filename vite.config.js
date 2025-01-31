cacfbb824201d2ba8cf2015cdff4e81519054f2e
catatsumuri <catatsumuri@gmail.com>
2025-01-31 11:42:47 +0900

    Apply Prettier formatting and refactor JSX structure

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.jsx',
-            refresh: true,
-        }),
-        react(),
-    ],
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        server: {
+            port: env.VITE_PORT || 5173, 
+            origin: `http://${env.VITE_HMR_HOST}:${env.VITE_PORT || 5173}`, 
+            cors: true, 
+            hmr: {
+                host: env.VITE_HMR_HOST || 'localhost', 
+            },
+        },
+        plugins: [
+            laravel({
+                input: 'resources/js/app.jsx',
+                refresh: true,
+            }),
+            react(),
+        ],
+    };
 });
